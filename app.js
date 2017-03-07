db260e45033af412cf6cfec4e4cf85ac186d0128
Veer Gangwal <veer.gangwal@gmail.com>
2017-03-06 20:11:37 -0800

    started app.js, added in press-release

---
+++ app.js
@@ -0,0 +1,2 @@
+const PANTHEON = ["The Beatles", "Kanye West", "Tame Impala", "Radiohead", "Frank Ocean", 
+                "The XX", "Kendrick Lamar", "Coldplay", "Beyonce", "Sam Cooke", "Marvin Gaye", "R. Kelly"]
